b7d42351f342b4b6bcad3f66bac109d16dfa2aed
ghlai9665 <glai9665@gmail.com>
2021-11-01 14:04:31 -0500

    allow learner to save callbacks after fit

--- octopus/learner.py
+++ octopus/learner.py
@@ -5,8 +5,8 @@ from fastai.basics import store_attr, noop
 import dill
 
 class Learner:
-    def __init__(self, model, dls, loss_func, optimizer, lr, valid_interval=2, cbs=[], get_pred=None):
-        store_attr('model, dls, loss_func, optimizer, lr, valid_interval, cbs, get_pred', self)
+    def __init__(self, model, dls, loss_func, optimizer, lr, valid_interval=2, cbs=[], get_pred=None, save_learner=True):
+        store_attr('model, dls, loss_func, optimizer, lr, valid_interval, cbs, get_pred, save_learner', self)
         for cb in cbs: cb.learner = self
         self.cb_dict = { type(cb).__name__: cb for cb in self.cbs}
 
@@ -24,6 +24,8 @@ class Learner:
                   self.validate_interval()
             self('after_epoch')
         self('after_fit')
+        if self.save_learner: 
+            self.save("./")
     
     def validate_interval(self):
         self.validate_model() 
@@ -49,7 +51,6 @@ class Learner:
         for batch_x, batch_y in self.dls.valid_dl:
             self.batch_x, self.batch_y = batch_x, batch_y
             self.validate_batch()
-
         self('after_validate')
 
     def validate_batch(self):
@@ -64,19 +65,16 @@ class Learner:
     def __call__(self,name):
         for cb in self.cbs: getattr(cb,name,noop)()
 
-    def export_attr_from_cb(self, cb, attrs: Set[str]):
-      return {key: cb.__dict__[key] for key in attrs}
-
-    def export(self, export_attrs: Dict[str, Set[str]]):
+    def save(self, path):
+      """
+      Save callback dict to `path`
+      """
       export = {}
-      for cb_name, attrs in export_attrs.items():
-        cb = self.cb_dict[cb_name]
-        export[cb_name] = self.export_attr_from_cb(cb, attrs)
-      return export 
-
-    def save(self, export_attrs, path):
-      export = self.export(export_attrs)
-      with open(f"{path}/mylearner.pkl", 'wb') as learner_file:
+      for cb_name, cb in self.cb_dict.items():
+        # export everything except for reference to learner
+        export[cb_name] = {key: value for key, value in cb.__dict__.items() if key != 'learner'}
+      # save
+      with open(f"{path}/learner.pkl", 'wb') as learner_file:
         dill.dump(export, learner_file)
         print("save successful!")
 
